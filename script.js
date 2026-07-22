06bec82ca29aa85342beff61b64a475acff5aa45
RiKi <gfatt2278@gmail.com>
2026-07-22 11:02:18 +0630

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ heart.onclick = function(){
     message.style.display = "block";
 
 };
+const heart=document.getElementById("heart");
+const msg=document.getElementById("message");
+
+heart.onclick=function(){
+
+msg.style.display="block";
+
+sparkle();
+
+flowers();
+
+}
