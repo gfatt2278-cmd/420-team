ec49adcdd8894c084145a2bb4ab2e040426f5016
RiKi <gfatt2278@gmail.com>
2026-07-22 11:03:31 +0630

    Implement sparkle function for animated sparkles
    Added a sparkle function that creates animated sparkles on the screen.

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ sparkle();
 flowers();
 
 }
+function sparkle(){
+
+for(let i=0;i<60;i++){
+
+let s=document.createElement("div");
+
+s.innerHTML="✨";
+
+s.style.position="absolute";
+
+s.style.left=Math.random()*window.innerWidth+"px";
+
+s.style.top=Math.random()*window.innerHeight+"px";
+
+s.style.fontSize="25px";
+
+document.body.appendChild(s);
+
+setTimeout(()=>{
+
+s.remove();
+
+},1500);
+
+}
+
+}
